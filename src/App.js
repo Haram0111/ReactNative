8821a79c72bb558ae5d5e29e6a6f1053df0d2bf7
Haram0111 <5gkfka5@naver.com>
2022-07-08 23:39:20 +0900

    E : input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { StatusBar, TouchableOpacity } from 'react-native-web';
 import styled, {ThemeProvider} from 'styled-components/native';
-import { theme } from './theme'
+import { theme } from './theme';
+import Input from './components/input';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({theme}) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({theme}) => theme.main};
+  align-self: flex-start;
+  margin: 0px 20px;
+`;
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container></Container>
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.background}/>
+        <Title>TODO List</Title>
+        <Input />
+      </Container>
     </ThemeProvider>
   );
 }
-
-const Container = styled.View`
-  flex: 1;
-  background-color: ${({theme}) => theme.background};
-  alignItems: center;
-  justifyContent: center;
-`;
