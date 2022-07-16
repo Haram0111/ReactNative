62b77fa74680906dee0cd2b47e1382924c00f589
Haram0111 <5gkfka5@naver.com>
2022-07-16 22:43:27 +0900

    A:style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View, StyleSheet } from "react-native";
-import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="light"/>
-      <View>
-        <Text>Seoul</Text>
-        <View></View>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>        
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descriptrion}>Sunny</Text>
+        </View>
       </View>
     </View>
   );
@@ -18,5 +21,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "tomato"
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    backgroundColor: "teal",
+    alignItems: "center",
+    flex: 1,
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  descriptrion: {
+    marginTop: -30,
+    fontSize: 60,
   }
 })
