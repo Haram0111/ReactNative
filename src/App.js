0bd0a3886a9ff8e60685d94b28df373d1cccd94c
Haram0111 <5gkfka5@naver.com>
2022-07-21 22:48:49 +0900

    A : WeatherAPI, icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import * as Location from 'expo-location';
 import { View, Text, StyleSheet, ScrollView, Dimensions, ActivityIndicator } from "react-native";
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
+import { Fontisto } from "@expo/vector-icons";
 
-const API_KEY = "fce3f24279fbed525432cb299d839c7d";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
+const API_KEY = "39cecfff4fc160b196d696e6671bfbb5";
 
 export default function App() {
 
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-
+  
   const getWeather = async() => {
     const {granted} = await Location.requestForegroundPermissionsAsync();
     if(!granted){
@@ -24,15 +34,18 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].region);
+
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=matric`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
     );
     const json = await response.json();
-    setDays(json.daliy);
+    setDays(json.daily);
+    console.log(json.daily);
   };
 
   useEffect(()=> {
     getWeather();
+
   },[]);
 
   return (
@@ -42,16 +55,39 @@ export default function App() {
       </View>
       <ScrollView showsHorizontalScrollIndicator={false} pagingEnabled horizontal contentContainerStyle={styles.weather}>
         {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator color="white"  style={{marginTop: 10}} size="large" />
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
           </View>
         ) : (
           days.map((day, index) => (
-          <View key={index} style={styles.day}>
-            <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
-            <Text style={styles.descriptrions}>{dqy.weather[0].main}</Text>
-          </View>))
-        )};
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
+              
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -82,8 +118,14 @@ const styles = StyleSheet.create({
     marginTop: 50,
     fontSize: 178,
   },
-  descriptrion: {
+  description: {
     marginTop: -30,
     fontSize: 60,
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500",
   }
 })
